4f2960bd0ebdd91061a79c20e2fba595c80db597
Aline-Ferreira1980 <aline.fn80@gmail.com>
2020-09-17 17:48:31 -0300

    Adicionando HTML e CSS no Angular(Components)

--- frontend-glace/glace/src/app/app.module.ts
+++ frontend-glace/glace/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PesquisaComponent } from './pesquisa/pesquisa.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PesquisaComponent,
+    QuemSomosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
