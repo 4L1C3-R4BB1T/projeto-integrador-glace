dee69a3e2cb4d82fcbf06d2478dcb0550207ed98
Aline-Ferreira1980 <aline.fn80@gmail.com>
2020-09-20 18:21:20 -0300

    correção link

--- frontend-glace/glace/src/app/app.module.ts
+++ frontend-glace/glace/src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,7 +32,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule, 
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
