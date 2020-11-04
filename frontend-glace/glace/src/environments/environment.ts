4e274b8a4a6bf2cbb29d742a4cb219acb8604625
Aline-Ferreira1980 <aline.fn80@gmail.com>
2020-11-04 17:48:18 -0300

    deploy front

--- frontend-glace/glace/src/environments/environment.ts
+++ frontend-glace/glace/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   logRequest: true,
   traceRequest: true,
-  URLSERVIDOR: 'http://localhost:8080/'
+  URLSERVIDOR: 'https://git.heroku.com/projetoglace.git/'
 };
 
 /*
