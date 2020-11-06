1a035ca940351577e976daa07b170b2fdf96877f
Aline-Ferreira1980 <aline.fn80@gmail.com>
2020-11-06 10:19:27 -0300

    Correção do perfil parceiro

--- frontend-glace/glace/src/app/parceiro-glace/parceiro-glace-routing.module.ts
+++ frontend-glace/glace/src/app/parceiro-glace/parceiro-glace-routing.module.ts
@@ -8,11 +8,11 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'perfilParceiro/:codigo', component: PerfilParceiroComponent,
     canActivate: [AuthGuard],
-    data: { roles: ['PG', 'UG'] }
+    data: { roles: ['PG'] }
   },
   { path: 'perfilParceiro', component: PerfilParceiroComponent,
     canActivate: [AuthGuard],
-    data: { roles: ['PG', 'UG'] }
+    data: { roles: ['PG'] }
   },
   {
     path: 'listar/parceiro', component: ParceiroTableComponent,
