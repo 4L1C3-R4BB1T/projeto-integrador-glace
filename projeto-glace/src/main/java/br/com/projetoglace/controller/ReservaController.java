7708a2393e4ca9b8ee8e64fff65758f8f434492e
Aline-Ferreira1980 <aline.fn80@gmail.com>
2020-11-17 18:20:37 -0300

    criação da classe reserva

--- projeto-glace/src/main/java/br/com/projetoglace/controller/ReservaController.java
+++ projeto-glace/src/main/java/br/com/projetoglace/controller/ReservaController.java
@@ -6,29 +6,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projetoglace.dto.ReservaDTO;
 import br.com.projetoglace.model.Reserva;
 import br.com.projetoglace.request.ReservaRequest;
 import br.com.projetoglace.service.ReservaService;
 
-@Controller
+@CrossOrigin
+@RestController
+@RequestMapping ("/reserva")
 public class ReservaController {
 
 	@Autowired
 	private ReservaService service;
 	
+	//@Override
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<?> salvarReserva(@RequestBody ReservaRequest reservaRequest) {	
 		try {
-			
 			ReservaDTO reservaDto = service.salvarReserva(reservaRequest);			
 			return ResponseEntity.status(HttpStatus.CREATED).body(reservaDto);
 		
@@ -37,7 +42,6 @@ public class ReservaController {
 		}		
 	}
 		
-	
 	@GetMapping
 	public List<Reserva> listarReserva(){
 		return service.listarReserva();
---
+++ projeto-glace/src/main/java/br/com/projetoglace/controller/openapi/ReservaControllerOpenApi.java
@@ -0,0 +1,5 @@
+package br.com.projetoglace.controller.openapi;
+
+public class ReservaControllerOpenApi {
+
+}
