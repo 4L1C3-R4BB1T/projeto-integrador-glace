2877788ce39d8bab78fa08932ad6cf25da6c10b4
Aline-Ferreira1980 <aline.fn80@gmail.com>
2020-11-18 13:46:32 -0300

    atualização do Quem somos

--- frontend-glace/glace/src/environments/environment.prod.ts
+++ frontend-glace/glace/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   logRequest: true,
   traceRequest: true,
-  URLSERVIDOR:'https://glace-backend.herokuapp.com/'
-  //URLSERVIDOR: 'http://localhost:8080/'
+  //URLSERVIDOR:'https://glace-backend.herokuapp.com/'
+  URLSERVIDOR: 'http://localhost:8080/'
 };
--- projeto-glace/src/main/java/br/com/projetoglace/controller/ReservaController.java
+++ projeto-glace/src/main/java/br/com/projetoglace/controller/ReservaController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,12 +32,10 @@ public class ReservaController {
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<?> salvarReserva(@RequestBody ReservaRequest reservaRequest) {	
-		
-		System.out.println(reservaRequest);
-		
 		try {
-			ReservaDTO reservaDTO = service.salvarReserva(reservaRequest);			
-			return ResponseEntity.status(HttpStatus.CREATED).body(reservaDTO);
+			ReservaDTO reservaDto = service.salvarReserva(reservaRequest);			
+			return ResponseEntity.status(HttpStatus.CREATED).body(reservaDto);
+		
 		}catch(Exception ex) {
 			return ResponseEntity.badRequest().body(ex.getMessage());
 		}		
@@ -58,5 +55,4 @@ public class ReservaController {
 			return ResponseEntity.notFound().build();
 		}
 	}
-
 }
