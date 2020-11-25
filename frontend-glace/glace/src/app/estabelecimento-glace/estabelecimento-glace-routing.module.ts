83391ee11d058dd08c064003dbe7d55520cde86c
Genesisgn10 <genesiscaquino.2016@gmail.com>
2020-11-25 16:46:50 -0300

    put estabelecimento

--- frontend-glace/glace/src/app/estabelecimento-glace/estabelecimento-glace-routing.module.ts
+++ frontend-glace/glace/src/app/estabelecimento-glace/estabelecimento-glace-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroLocalComponent } from './cadastro-local/cadastro-local.component';
 import { DetalhesLocalComponent } from './detalhes-local/detalhes-local.component';
 
 const routes: Routes = [
-  { path: 'detalhes/local/:codigo', component: DetalhesLocalComponent }
+  { path: 'detalhes/local/:codigo', component: DetalhesLocalComponent },
+  { path: 'estabelecimento/:codigo', component: CadastroLocalComponent }
+
 ];
 
 @NgModule({
